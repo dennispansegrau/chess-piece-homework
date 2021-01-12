90164f78d58cc69ed4bf85a9a3d572e7c98991ed
dennispansegrau <dennis@pansegrau.biz>
2021-01-12 16:57:23 +0100

    command line

--- src/Command/ChessCommand.php
+++ src/Command/ChessCommand.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use Exception;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -13,22 +15,61 @@ class ChessCommand extends Command
 
     protected function configure(): void
     {
-        // ...
+        $this
+            ->setDescription('Shows all possible moves of a chosen chess piece.')
+            ->addArgument(
+                'chess_piece',
+                InputArgument::OPTIONAL,
+                'Chess piece (K-King, Q-Queen, R-Rook, B-Bishop, N-Knight, or P-Pawn)',
+                $this->getRandomChessPiece()
+            )
+            ->addArgument(
+                'position',
+                InputArgument::OPTIONAL,
+                'Chess piece position (a1 - h8)',
+                $this->getRandomPosition()
+            )
+            ->setHelp('Shows all possible moves of a chosen chess piece.')
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // ... put here the code to create the user
+        /** @var string $chessPiece */
+        $chessPiece = $input->getArgument('chess_piece');
+        /** @var string $position */
+        $position = $input->getArgument('position');
 
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
+        $output->writeln([
+            'Chess Piece Homework',
+            '========================================',
+            "Selected chess piece: $chessPiece",
+            "Selected chess piece position: $position",
+            '========================================',
+        ]);
 
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
+        $output->writeln('Whoa!');
+
         return Command::SUCCESS;
+    }
 
-        // or return this if some error happened during the execution
-        // (it's equivalent to returning int(1))
-        // return Command::FAILURE;
+    /**
+     * @return string
+     */
+    private function getRandomPosition(): string
+    {
+        try {
+            return chr(random_int(97, 104)) . (string)random_int(1, 8);
+        } catch (Exception $e) {
+            return 'a1';
+        }
+    }
+
+    /**
+     * @return string
+     */
+    private function getRandomChessPiece(): string
+    {
+        return array_rand(array_flip(['K', 'Q', 'R', 'B', 'N', 'P']));
     }
 }
